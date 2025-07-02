22dc01e7fc602bb5498fe74e03c40cc1d156936a
ShrutiSugandhi <shrutisugandhi00@gmail.com>
2025-07-02 21:52:07 +0530

    Squashed commits from otherOperation

--- Git-Task-2/BaseFile.java
+++ Git-Task-2/BaseFile.java
@@ -8,13 +8,21 @@ public class BaseFile {
     public int add(int a, int b, int c) {
         System out (“This is add method for 3 params”);
         return a + b + c;
+        }
+    public int sub(int a, int b) {
+        return a - b;
     }
 
-    public int add(int a, int b, int c, int d) {
-        System out (“This is add method for 4 params”);
-        return a + b + c + d;
+    public int div(int a, int b) {
+        return a / b;
     }
 
+    public int mult(int a, int b) {
+        return a * b;
+    }
+    public int sqrt(int N) {
+        return Math.sqrt(N);
+    }
     public static void main(String[] args) {
 
     }
