79e572228c356dfa947fb1a6d0473f532380be57
ShrutiSugandhi <shrutisugandhi00@gmail.com>
2025-07-02 22:16:48 +0530

    Add 3-param add method in advOperstion branch

--- Git-Task-2/BaseFile.java
+++ Git-Task-2/BaseFile.java
@@ -1,8 +1,13 @@
 
 public class BaseFile {
     public int add(int a, int b) {
-        System out (“This is add method”);
+        System out (“This is add method for 2 params”);
         return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        System out (“This is add method  for 3 params”);
+        return a + b + c;
     }
 
     public static void main(String[] args) {
